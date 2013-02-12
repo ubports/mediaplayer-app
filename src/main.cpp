204e98252412c90d21c8da192d20e2b76fc3e208
Jim Hodapp <jim.hodapp@canonical.com>
2013-02-12 09:30:57 -0500

    Got rid of original author names/email addresses from source files as this isn't necessary.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 /*
  * Copyright (C) 2012-2013 Canonical, Ltd.
  *
- * Authors:
- *  Ugo Riboni <ugo.riboni@canonical.com>
- *  Michał Sawicz <michal.sawicz@canonical.com>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
--- src/thumbnail-pipeline-gst.cpp
+++ src/thumbnail-pipeline-gst.cpp
@@ -1,9 +1,6 @@
 /*
  * Copyright (C) 2013 Canonical, Ltd.
  *
- * Authors:
- *  Renato Araujo Oliveira Filho <renato@canonical.com>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
